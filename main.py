4d3403da8c5803be0080177f606bf799ab2e8537
ExceptionCoder <admin@exceptioncoder.com>
2017-08-19 13:44:34 +0530

    hello world

---
+++ main.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore, QtWidgets, uic
+import os,sys
+
+BASE_FOLDER = os.path.dirname(os.path.realpath(__file__))
+UI_FOLDER = os.path.join(BASE_FOLDER,"ui")
+
+main_window_ui_file = os.path.join(UI_FOLDER,"main.ui")
+main_window_uic = uic.loadUiType(main_window_ui_file)[0] ## Get Ui Class (Form Class)
+
+
+class Calculator(QtWidgets.QMainWindow,main_window_uic):
+
+    def __init__(self):
+        super(Calculator, self).__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = Calculator()
+    main_window.show()
+    sys.exit(app.exec_())
